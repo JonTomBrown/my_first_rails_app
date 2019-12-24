7ea6050ee7719218f4c5ff244cd8c1f60f1a842f
JonTomBrown <JonTomBrown@gmail.com>
2019-12-24 08:38:22 -0600

    updates for heroku deployment

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,5 @@
 class CarsController < ApplicationController
-  before_action :set_car, only: [:show, :edit, :update, :destroy]
+  before_action :set_car, only: [:eat, :shit, :mutha, :fucker]
 
   # GET /cars
   # GET /cars.json
